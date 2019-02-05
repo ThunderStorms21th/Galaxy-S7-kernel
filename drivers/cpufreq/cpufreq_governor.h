457bcb4cfdb38b4a3379252f669282d81cd7ffe1
ThunderStorms21th <pinakastorm@gmail.com>
2019-02-05 15:33:52 +0100

    added support for edit CPU conservative gov

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -193,7 +193,7 @@ struct dbs_data;
 struct common_dbs_data {
 	/* Common across governors */
 	#define GOV_ONDEMAND		0
-	#define GOV_CONSERVATIVE	1
+	#define GOV_CONSERVATIVE	0
 	int governor;
 	struct attribute_group *attr_group_gov_sys; /* one governor - system */
 	struct attribute_group *attr_group_gov_pol; /* one governor - policy */
